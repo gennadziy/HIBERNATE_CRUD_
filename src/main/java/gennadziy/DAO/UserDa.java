7f0e1bd2701a1e31fe60e0424d9e89cebf130309
MEDION <geniy@tut.by>
2020-01-22 21:45:32 +0100

    first commit

---
+++ src/main/java/gennadziy/DAO/UserDa.java
@@ -0,0 +1,4 @@
+package gennadziy.DAO;
+
+public interface UserDa {
+}
---
+++ src/main/java/gennadziy/service/UserServ.java
@@ -0,0 +1,10 @@
+package gennadziy.service;
+
+/*
+@Author Gennadziy GITHUB/gennadziy
+Class name: UserServ
+Date: 2020-01-22
+Time: 21:33
+*/
+public class UserServ {
+}
